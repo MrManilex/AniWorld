a73e3b7bc13974f4cb1390957c8dbe265c6cda11
WangManLex <alexandermaniwang99@gmail.com>
2021-11-08 09:37:34 -0500

    anime model done

--- models/anime.js
+++ models/anime.js
@@ -2,7 +2,11 @@ import mongoose from 'mongoose'
 const Schema = mongoose.Schema
 
 const animeSchema = new Schema({
-
+  title: {type: String, required: true},
+  anilist_id: Number,
+  cover_image: String,
+  collectedBy: [{type: Schema.Types.ObjectId, ref: 'Profile'}],
+  reviews: [{type: Schema.Types.ObjectId, ref: 'AnimeReview'}]
 })
 
 const Anime = mongoose.models('Anime', animeSchema)
