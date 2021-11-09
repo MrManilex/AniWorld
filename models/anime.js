fd6e42cd7660122ab106d7782d9a312a131218cd
WangManLex <alexandermaniwang99@gmail.com>
2021-11-08 22:09:26 -0500

    rename anilistId to animeId for better understanding and add Add to List button for individual anime

--- models/anime.js
+++ models/anime.js
@@ -3,7 +3,7 @@ const Schema = mongoose.Schema
 
 const animeSchema = new Schema({
   animeTitle: {type: String, required: true},
-  anilistId: Number,
+  animeId: Number,
   coverImage: String,
   collectedBy: [{type: Schema.Types.ObjectId, ref: 'Profile'}]
   // reviews: [{type: Schema.Types.ObjectId, ref: 'AnimeReview'}]
