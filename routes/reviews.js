952bc90193332c61645b3c35fe04cad0149e5421
WangManLex <alexandermaniwang99@gmail.com>
2021-11-11 15:40:18 -0500

    check if user is logged into google

--- routes/reviews.js
+++ routes/reviews.js
@@ -1,4 +1,13 @@
 import { Router } from "express";
 const router = Router()
 
-router.get('/:id', reviewsCtrl.create)
+router.get('/:id',isLoggedIn, reviewsCtrl.create)
+
+function isLoggedIn(req, res, next) {
+  if (req.isAuthenticated()) return next();
+  res.redirect("/auth/google");
+}
+
+export{
+  router
+}
