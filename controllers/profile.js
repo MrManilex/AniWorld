a2f1713055bc1ff9d4e3a6281beb4504854de887
WangManLex <alexandermaniwang99@gmail.com>
2021-11-11 23:36:40 -0500

    stub profile show function

--- controllers/profile.js
+++ controllers/profile.js
@@ -1,10 +1,21 @@
 import { Profile } from "../models/profile.js";
+import { Review } from '../models/animeReview.js'
+import { Anime } from '../models/anime.js'
 
 function show(req, res){
   Profile.findById(req.params.id)
   .then(profile => {
-    res.render('profile/show', {
-      profile
+    Review.find({ author: profile._id })
+    .then(reviews => {
+      Anime.find({ collectedBy: profile._id})
+      .then(animes => {
+        res.render('profile/show', {
+          title: 'Test',
+          profile,
+          reviews,
+          animes
+        })
+      })
     })
   })
 }
