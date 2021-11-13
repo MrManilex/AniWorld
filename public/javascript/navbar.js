e65c0187d4c52f696cc3be21bce53718fbfdbb7c
WangManLex <alexandermaniwang99@gmail.com>
2021-11-13 12:05:14 -0500

    add functionality to navburger

---
+++ public/javascript/navbar.js
@@ -0,0 +1,6 @@
+const burgerIcon = document.getElementById('burger')
+const navbarMenu = document.getElementById('nav-links')
+
+burgerIcon.addEventListener('click', () => {
+  navbarMenu.classList.toggle('is-active')
+})
